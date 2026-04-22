9b06e271b8cd5157702ff93599e9b4439a5386d8
Enrico <enrico.puglisi@gmail.com>
2026-04-22 16:16:20 +0200

    new words added

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'svverbs-v9';
+const CACHE = 'svverbs-v10';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json', './verbs.csv', './vocabulary.csv', './grammar.html'];
 
 self.addEventListener('install', e => {
